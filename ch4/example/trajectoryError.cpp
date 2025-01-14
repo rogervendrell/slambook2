be16373095b8f5d84facfb9e9a3dcfa9dda2e7b3
Roger Vendrell <47077975+rogervendrell@users.noreply.github.com>
2025-01-14 14:28:27 +0100

    Update ch4

--- ch4/example/trajectoryError.cpp
+++ ch4/example/trajectoryError.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <unistd.h>
 #include <pangolin/pangolin.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
 #include <sophus/se3.hpp>
 
 using namespace Sophus;
@@ -10,7 +11,8 @@ using namespace std;
 string groundtruth_file = "./example/groundtruth.txt";
 string estimated_file = "./example/estimated.txt";
 
-typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
+typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>>
+    TrajectoryType;
 
 void DrawTrajectory(const TrajectoryType &gt, const TrajectoryType &esti);
 
@@ -48,7 +50,8 @@ TrajectoryType ReadTrajectory(const string &path) {
   while (!fin.eof()) {
     double time, tx, ty, tz, qx, qy, qz, qw;
     fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
-    Sophus::SE3d p1(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz));
+    Sophus::SE3d p1(Eigen::Quaterniond(qw, qx, qy, qz),
+                    Eigen::Vector3d(tx, ty, tz));
     trajectory.push_back(p1);
   }
   return trajectory;
@@ -63,13 +66,12 @@ void DrawTrajectory(const TrajectoryType &gt, const TrajectoryType &esti) {
 
   pangolin::OpenGlRenderState s_cam(
       pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
-      pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
-  );
+      pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0));
 
   pangolin::View &d_cam = pangolin::CreateDisplay()
-      .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
-      .SetHandler(new pangolin::Handler3D(s_cam));
-
+                              .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175),
+                                         1.0, -1024.0f / 768.0f)
+                              .SetHandler(new pangolin::Handler3D(s_cam));
 
   while (pangolin::ShouldQuit() == false) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -96,7 +98,6 @@ void DrawTrajectory(const TrajectoryType &gt, const TrajectoryType &esti) {
       glEnd();
     }
     pangolin::FinishFrame();
-    usleep(5000);   // sleep 5 ms
+    usleep(5000);  // sleep 5 ms
   }
-
 }
--- ch4/useSophus.cpp
+++ ch4/useSophus.cpp
@@ -1,8 +1,9 @@
-# include <iostream>
-# include <cmath>
-# include <Eigen/Core>
-# include <Eigen/Geometry>
-# include "sophus/se3.hpp"
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <cmath>
+#include <iostream>
+
+#include "sophus/se3.hpp"
 
 using namespace std;
 using namespace Eigen;
@@ -10,7 +11,7 @@ using namespace Eigen;
 /// 本程序演示sophus的基本用法
 int main(int argc, char **argv) {
   // 沿Z轴转90度的旋转矩阵
-  Matrix3d R = AngleAxisd(M_PI/2,Vector3d(0,0,1)).toRotationMatrix();
+  Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
   // 或者四元数
   Quaterniond q(R);
   Sophus::SO3d SO3_R(R);  // Sophus::SO3d可以直接从旋转矩阵构造
@@ -26,11 +27,11 @@ int main(int argc, char **argv) {
   // hat为向量到反对称矩阵
   cout << "so3 hat=\n" << Sophus::SO3d::hat(so3) << endl;
   // 相对的，vee为反对称到向量
-  cout << "so3 hat vee= " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
+  cout << "so3 hat vee= "
+       << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
 
   // 增量扰动模型的更新
-  Vector3d update_so3(1e-4,0,0); //假设更新量为这么多
-
+  Vector3d update_so3(1e-4, 0, 0);  // 假设更新量为这么多
 
   return 0;
 }
